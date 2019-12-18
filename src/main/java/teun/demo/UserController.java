5f037620348a9dce5c56a209d585041642ef5e35
Vuurtoren2000 <chris.nooteboom@gmail.com>
2019-12-18 17:51:22 +0100

    refresh commit

---
+++ src/main/java/teun/demo/UserController.java
@@ -0,0 +1,18 @@
+package teun.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/showall")
+    public String showAllUsers() {
+        return "showall";
+    }
+
+    @GetMapping("/new")
+    public String createNewUser() {
+        return "newUserForm";
+    }
+}
