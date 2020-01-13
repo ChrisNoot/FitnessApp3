2328abf134edf2dc32d265b6ae0e33978b0f6c10
Vuurtoren2000 <chris.nooteboom@gmail.com>
2020-01-13 22:40:49 +0100

    bullshit commit

--- src/main/java/teun/demo/controller/ExerciseController.java
+++ src/main/java/teun/demo/controller/ExerciseController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import teun.demo.domain.Exercise;
 import teun.demo.repository.ExerciseFactRepository;
 import teun.demo.repository.ExerciseRepository;
 
 @Controller
 @RequestMapping("/Categories")
+@SessionAttributes("chosenCategory")
 public class ExerciseController {
 
     private ExerciseFactRepository exerciseFactRepository;
@@ -19,6 +22,11 @@ public class ExerciseController {
     public ExerciseController(ExerciseFactRepository exerciseFactRepo) {
         this.exerciseFactRepository = exerciseFactRepo;
     }
+
+    public String showSubCategories(@ModelAttribute()) {
+    }
+
+    public void processCategory()
 
     @ModelAttribute(name = "chosenCategory")
     public Exercise.Category returnCategory() {
