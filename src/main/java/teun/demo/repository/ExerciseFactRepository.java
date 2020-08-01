243edc0a1d903fd8d567e2688ed227344047283d
Vuurtoren2000 <chris.nooteboom@gmail.com>
2020-08-01 11:35:18 +0200

    Added insert method to ExerciseFactRepo

--- src/main/java/teun/demo/repository/ExerciseFactRepository.java
+++ src/main/java/teun/demo/repository/ExerciseFactRepository.java
@@ -1,7 +1,17 @@
 package teun.demo.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import teun.demo.domain.ExerciseFact;
 
-public interface ExerciseFactRepository extends CrudRepository<ExerciseFact,Long> {
+public interface ExerciseFactRepository extends CrudRepository<ExerciseFact, Long> {
+
+    @Query(
+            value = "INSERT INTO EXERCISE_FACT_TABLE values (user_id = :userId, exercise_id = :exerciseId, score = :score",
+            nativeQuery = true)
+    void insertNewExerciseFactUserIdExerciseIdScore(@Param("userId") Long userId,
+                                                    @Param("exerciseId") Long exerciseId,
+                                                    @Param("score") Long score);
+
 }
