55518a0898681b486d39f4d47f5a23652682dbfb
Vuurtoren2000 <chris.nooteboom@gmail.com>
2020-08-01 11:36:08 +0200

    checkstyle again

--- src/main/java/teun/demo/repository/UserRepository.java
+++ src/main/java/teun/demo/repository/UserRepository.java
@@ -1,15 +1,8 @@
 package teun.demo.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import teun.demo.domain.Group;
 import teun.demo.domain.User;
 
-import javax.persistence.TypedQuery;
-import java.util.List;
-
-public interface UserRepository extends CrudRepository<User,Long> {
-
+public interface UserRepository extends CrudRepository<User, Long> {
 
 }
