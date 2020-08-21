462c989d63a59fc48009d2e36e6dbb0385aec110
cnooteboom <cnooteboom@degiro.nl>
2020-08-21 09:20:58 +0200

    Query refactoring

--- src/main/java/teun/demo/repository/ExerciseFactRepository.java
+++ src/main/java/teun/demo/repository/ExerciseFactRepository.java
@@ -3,15 +3,17 @@ package teun.demo.repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import teun.demo.domain.ExerciseFact;
+
+import java.util.Collection;
 
 public interface ExerciseFactRepository extends CrudRepository<ExerciseFact, Long> {
 
     @Query(
-            value = "INSERT INTO EXERCISE_FACT_TABLE values (user_id = :userId, exercise_id = :exerciseId, score = :score",
+            value = "INSERT INTO EXERCISE_FACT (user_id, exercise_id, score) VALUES (:userId, :exerciseId, :score)",
             nativeQuery = true)
     void insertNewExerciseFactUserIdExerciseIdScore(@Param("userId") Long userId,
                                                     @Param("exerciseId") Long exerciseId,
                                                     @Param("score") Long score);
-
 }
