2d7112a351fd30e32116eb70fded8a908f97121a
cnooteboom <cnooteboom@degiro.nl>
2020-09-21 21:34:02 +0200

    Added new method in repository

--- src/main/java/teun/demo/repository/ExerciseFactRepository.java
+++ src/main/java/teun/demo/repository/ExerciseFactRepository.java
@@ -3,17 +3,14 @@ package teun.demo.repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 import teun.demo.domain.ExerciseFact;
 
-import java.util.Collection;
+import java.util.List;
 
 public interface ExerciseFactRepository extends CrudRepository<ExerciseFact, Long> {
 
     @Query(
-            value = "INSERT INTO EXERCISE_FACT (user_id, exercise_id, score) VALUES (:userId, :exerciseId, :score)",
+            value = "select * from exercise_fact where user_id = :userId and exercise_id = :exerciseId",
             nativeQuery = true)
-    void insertNewExerciseFactUserIdExerciseIdScore(@Param("userId") Long userId,
-                                                    @Param("exerciseId") Long exerciseId,
-                                                    @Param("score") Long score);
+    List<ExerciseFact> findExerciseFactByUserIdAndExerciseId(@Param("userId") Long userId, @Param("exerciseId") Long exerciseId);
 }
